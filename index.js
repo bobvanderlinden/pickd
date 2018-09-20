cf39129e836ca6e9908e0025251783ff7ddca85d
Bob van der Linden <bobvanderlinden@gmail.com>
2018-09-20 23:11:18 +0200

    add chord filtering

--- index.js
+++ index.js
@@ -9,8 +9,45 @@ const client = new Client({
 const app = new Koa();
 const router = new Router();
 
+function where(conditions) {
+  conditions = conditions.filter(condition => condition)
+  if (conditions.length === 0) {
+    return ''
+  } else {
+    return `where ${conditions.join(' and ')}`
+  }
+}
+
+function addSqlParameter(sqlParameters, value) {
+  sqlParameters.push(value)
+  const parameterNr = sqlParameters.length
+  return `$${parameterNr}`
+}
+
+function array(query) {
+  return query === null || query === undefined
+    ? query
+    : query instanceof Array
+    ? query
+    : [query]
+}
+
 router.get('/api/chords', async (ctx, next) => {
-  const result = await client.query("select ID, info->'artist_name' artist_name, info->'song_name' song_name, tab_content lyrics from chords limit 100")
+  const sqlParameters = []
+  const sqlParameter = addSqlParameter.bind(null, sqlParameters)
+  const sql = `
+    select ID, info->'artist_name' artist_name, info->'song_name' song_name, tab_content lyrics
+    from chords songs
+    ${where([
+      ctx.query.chord && (() => {
+        const chordSqlParameters = array(ctx.query.chord).map(sqlParameter).join(', ')
+        return `exists (select chord_code from song_chords where song_id = songs.id and song_chords.chord_code in (${chordSqlParameters})) and not exists (select chord_code from song_chords where song_id = songs.id and song_chords.chord_code not in (${chordSqlParameters}))`
+      })()
+    ])}
+    limit 100
+  `
+  console.log(sql, sqlParameters)
+  const result = await client.query(sql, sqlParameters)
   ctx.response.body = result.rows
 });
 
@@ -31,13 +68,16 @@ app
 
 async function init() {
   await client.connect()
-  await app.listen(
-    process.env.PORT
-      ? parseInt(process.env.PORT)
-      : 3000
-  )
+  const port = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 3000
+  await app.listen(port)
+  console.log('Listening on port', port)
 }
 
-init().then().catch(err => {
+init().then(result => {
+  console.log(result)
+}).catch(err => {
   console.error(err)
+  throw err
 })
